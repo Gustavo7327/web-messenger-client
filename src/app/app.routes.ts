0071bc2d9f407a3c7e99a398ac1a435b6284b1b5
Gustavo Chaves <gustavosilva712493@gmail.com>
2026-06-08 01:06:08 -0300

    feat: update routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,10 @@ import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { VerifyCodeComponent } from './pages/verify-code/verify-code.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ContactsComponent } from './pages/contacts/contacts.component';
+import { GroupsComponent } from './pages/groups/groups.component';
+import { SendersComponent } from './pages/senders/senders.component';
+import { LayoutComponent } from './components/layout/layout.component';
 import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
@@ -11,6 +15,16 @@ export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'verify', component: VerifyCodeComponent },
-  { path: 'dashboard', component: DashboardComponent, canActivate: [authGuard] },
+  {
+    path: '',
+    component: LayoutComponent,
+    canActivate: [authGuard],
+    children: [
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'contacts', component: ContactsComponent },
+      { path: 'groups', component: GroupsComponent },
+      { path: 'senders', component: SendersComponent },
+    ]
+  },
   { path: '**', redirectTo: '' }
 ];
